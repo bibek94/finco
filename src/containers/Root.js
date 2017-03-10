b1fd2406358ac7736c82d70396d37f58332bf4b8
Ashish Acharya <anarchyrucks@live.com>
2017-03-11 00:55:57 +0530

    Initial commit

---
+++ src/containers/Root.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react'
+import {Router} from 'react-router'
+import { Provider } from 'react-redux'
+
+import App from '../components/App'
+import routes from './routes'
+
+class Root extends Component {
+  render() {
+    const {store, history} = this.props
+    return (
+      <Provider store={store}>
+        <Router history={history} routes={routes}>
+        </Router>
+      </Provider>
+    )
+  }
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+}
+
+export default Root
